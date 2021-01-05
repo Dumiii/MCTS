74614ed676d7a26b95af2e68b04d1ab018f88a9e
SomeoneWasTaken <jpontes1998@gmail.com>
2021-01-05 15:33:53 +0000

    added Tree javadoc

--- MCTS/src/Tree.java
+++ MCTS/src/Tree.java
@@ -1,24 +1,46 @@
-
+/**
+ * Game tree that holds all States
+ */
 public class Tree {
-    MCTS.State root;
+	MCTS.State root;
 
-    public Tree() {
-        root = new MCTS.State();
-    }
+	/**
+	 * Default constructor that creates a empty root that is set after tree creation
+	 */
+	public Tree() {
+		root = new MCTS.State();
+	}
 
-    public Tree(MCTS.State root) {
-        this.root = root;
-    }
+	/**
+	 * Constructor that accepts a state to be root
+	 * @param root State to be set as the top of the tree
+	 */
+	public Tree(MCTS.State root) {
+		this.root = root;
+	}
 
-    public MCTS.State getRoot() {
-        return root;
-    }
+	/**
+	 * Get root method
+	 * @return root of the tree instance
+	 */
+	public MCTS.State getRoot() {
+		return root;
+	}
 
-    public void setRoot(MCTS.State root) {
-        this.root = root;
-    }
+	/**
+	 * Set root method
+	 * @param root State to be set as the root of the tree
+	 */
+	public void setRoot(MCTS.State root) {
+		this.root = root;
+	}
 
-    public void addChild(MCTS.State parent, MCTS.State child) {
-        parent.getChildArray().add(child);
-    }
+	/**
+	 * Adds the child state to the parent state's child array
+	 * @param parent
+	 * @param child
+	 */
+	public void addChild(MCTS.State parent, MCTS.State child) {
+		parent.getChildArray().add(child);
+	}
 }
